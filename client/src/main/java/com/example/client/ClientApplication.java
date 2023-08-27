5cd99e3641ab1725ac2e17c9a0db053a293fe0db
bhanukarandeniya <bhanukarandeniya@gmail.com>
2023-08-27 17:36:14 +0800

    Graphql implementation

--- client/src/main/java/com/example/client/ClientApplication.java
+++ client/src/main/java/com/example/client/ClientApplication.java
@@ -7,13 +7,18 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class ClientApplication {
@@ -29,14 +34,38 @@ public class ClientApplication {
 
     @Bean
     CustomerClient customerClient(WebClient.Builder builder) {
-        return HttpServiceProxyFactory.builder(WebClientAdapter.forClient(builder.baseUrl("http://localhost:8080").build())).build()
-                .createClient(CustomerClient.class);
+        return HttpServiceProxyFactory.builder(WebClientAdapter.forClient(builder.baseUrl("http://localhost:8080").build())).build().createClient(CustomerClient.class);
     }
 
     @Bean
     RouteLocator gateway(RouteLocatorBuilder rlb) {
-        return rlb.routes().route(rs -> rs.path("/proxy").filters(fs -> fs.setPath("/customers")
-                .retry(3).addResponseHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*")).uri("http://localhost:8080")).build();
+        return rlb.routes().route(rs -> rs.path("/proxy").filters(fs -> fs.setPath("/customers").retry(3).addResponseHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*")).uri("http://localhost:8080")).build();
+    }
+
+}
+
+@Controller
+class CustomerGraphqlController {
+
+    private final CustomerClient customerClient;
+
+    public CustomerGraphqlController(CustomerClient customerClient) {
+        this.customerClient = customerClient;
+    }
+
+    @QueryMapping
+    Flux<Customer> customers() {
+        return this.customerClient.all();
+    }
+
+    @QueryMapping
+    Mono<Customer> byId(@Argument Integer id) {
+        return this.customerClient.byId(id);
+    }
+
+    @SchemaMapping(typeName = "Customer")
+    Mono<Profile> profile(Customer customer) {
+        return Mono.just(new Profile(customer.id()));
     }
 
 }
@@ -47,10 +76,13 @@ interface CustomerClient {
     Flux<Customer> all();
 
     @GetExchange("/customers/{id}")
-    Flux<Customer> byId(@PathVariable Integer id);
+    Mono<Customer> byId(@PathVariable Integer id);
 
 }
 
+record Profile(Integer id) {
+
+}
 
 record Customer(Integer id, String name) {
 
